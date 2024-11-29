f71890494cd9906d3b37118018434b3b29f51074
Andrew Helsby <ajhelsby@hotmail.com>
2024-11-29 16:29:55 +0400

    feat(#5): Add cloudformation requirements

--- apps/common/integrations/aws/dynamodb/clients.py
+++ apps/common/integrations/aws/dynamodb/clients.py
@@ -10,10 +10,10 @@ class DynamoClient(DBClient):
     def __init__(self):
         self.client = boto3.client(
             "dynamodb",
-            endpoint_url="http://localhost:8090",  # DynamoDB Local endpoint
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-            region_name=os.getenv("AWS_REGION"),
+            # endpoint_url="http://localhost:8090",  # DynamoDB Local endpoint
+            # aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+            # aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+            # region_name=os.environ.get("AWS_REGION"),
         )
 
     def add(self, table_name: str, item: {}):
--- apps/common/models/base_model.py
+++ apps/common/models/base_model.py
@@ -1,12 +1,15 @@
 from dataclasses_json import dataclass_json
 
 from apps.common.db.connections import get_db_client
+from apps.common.logging import get_logger
 
+logger = get_logger(__name__)
 
 @dataclass_json
 class BaseModel:
     def __init__(self):
         self.client = get_db_client()
+        logger.info(self.client.__class__)
 
     def get(self, key) -> "BaseModel":
         raise NotImplementedError
